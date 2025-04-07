19c986935c95ed06cf12de4ab462e4160d2c29bd
Cleianderson <cleianderson13@gmail.com>
2025-04-07 22:48:52 +0000

    fix: StatusBar color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,30 @@
-import { ThemeProvider } from '@react-navigation/native';
-import { Stack } from "expo-router";
-import { Provider } from "react-redux";
+import { ThemeProvider } from '@react-navigation/native'
+import { Stack } from 'expo-router'
+import { Provider } from 'react-redux'
 
-import { Themes } from '@/constants/Theme';
-import { useColorScheme } from '@/hooks/useColorScheme';
-import store from "@/store";
-import Toast from '@/components/Toast';
-import ToastProvider from '@/providers/Toast';
+import { Themes } from '@/constants/Theme'
+import { useColorScheme } from '@/hooks/useColorScheme'
+import store from '@/store'
+import Toast from '@/components/Toast'
+import ToastProvider from '@/providers/Toast'
+import { StatusBar } from 'react-native'
 
 // import Requesting from "@/components/Requesting"
 // import Config from "@/providers/Config"
 
 function ThemeContainer() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme()
   return (
     <ThemeProvider value={colorScheme === 'dark' ? Themes.dark : Themes.light}>
+      <StatusBar
+        barStyle={colorScheme === 'dark' ? 'light-content' : 'dark-content'}
+        backgroundColor={colorScheme === 'dark' ? '#232323' : '#f0f0f0'}
+      />
       <Stack>
-        <Stack.Screen name='main' options={{ headerShown: false }} />
-        <Stack.Screen name='+not-found' />
-        <Stack.Screen name='warnings' options={{ title: 'Avisos' }} />
-        <Stack.Screen name='configs' options={{ title: 'Configurações' }} />
+        <Stack.Screen name="main" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="warnings" options={{ title: 'Avisos' }} />
+        <Stack.Screen name="configs" options={{ title: 'Configurações' }} />
       </Stack>
       <Toast />
     </ThemeProvider>
@@ -27,7 +32,6 @@ function ThemeContainer() {
 }
 
 export default function RootLayout() {
-
   return (
     <Provider store={store}>
       <ToastProvider>
