613a9242c0ce36eb3a3ea34c060a986e1b6d1cac
Cleianderson <cleianderson13@gmail.com>
2025-04-07 18:21:04 +0000

    feat: splashscreen and icon

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Redirect } from 'expo-router';
-import { useCallback, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { Sagas } from '@/constants/Sagas';
+import { Redirect } from 'expo-router'
+import { useCallback, useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import * as SplashsScreen from 'expo-splash-screen'
 
+import { Sagas } from '@/constants/Sagas'
+
+SplashsScreen.preventAutoHideAsync()
+SplashsScreen.setOptions({
+  duration: 1000,
+  fade: true,
+})
 
 export default function RootApp() {
   const [isOnboarded, setIsOnboarded] = useState(true)
+  const [isAppReady, setIsAppReady] = useState(false)
 
   const dispatch = useDispatch()
-
-
 
   useEffect(() => {
     let _day = new Date(Date.now()).getDay() - 1
@@ -20,15 +26,23 @@ export default function RootApp() {
     dispatch(Sagas.getFavorites())
     dispatch(Sagas.getWeek())
     dispatch(Sagas.getWarnings())
+
+    setIsAppReady(true)
   }, [])
 
+  useEffect(() => {
+    if (isAppReady) {
+      SplashsScreen.hide()
+    }
+  }, [isAppReady])
 
   const _app = useCallback(() => {
-    if (isOnboarded) {
-      return <Redirect href='/main' />
-    }
+    if (isAppReady === false) return null
 
-  }, [isOnboarded])
+    if (isOnboarded) {
+      return <Redirect href="/main" />
+    }
+  }, [isOnboarded, isAppReady])
 
   return _app()
 }
