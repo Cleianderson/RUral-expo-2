be3a9fbda7727da2f4cd09f7b80f9fcdea742c46
Cleianderson <cleianderson13@gmail.com>
2025-04-17 15:01:20 +0000

    fix: colorScheme on web

--- hooks/useColorScheme.web.ts
+++ hooks/useColorScheme.web.ts
@@ -1,20 +1,26 @@
 import { useEffect, useState } from 'react';
 import { useColorScheme as useRNColorScheme } from 'react-native';
+import { useSelector } from 'react-redux';
 
 /**
  * To support static rendering, this value needs to be re-calculated on the client side for web
  */
 export function useColorScheme() {
+  const { colorScheme } = useSelector<RootState, Configurations>(state => state.mainState.configurations)
   const [hasHydrated, setHasHydrated] = useState(false);
 
   useEffect(() => {
     setHasHydrated(true);
   }, []);
 
-  const colorScheme = useRNColorScheme();
+  const rnColorScheme = useRNColorScheme();
 
-  if (hasHydrated) {
+  if (hasHydrated && colorScheme) {
     return colorScheme;
+  }
+
+  if (colorScheme === null) {
+    return rnColorScheme
   }
 
   return 'light';
