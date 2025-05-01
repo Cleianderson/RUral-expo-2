5640fcfc0b7ab810fb5faeb0e7a43759c1aba174
Cleianderson Paz <cleianderson13@gmail.com>
2025-05-01 14:17:36 -0300

    refact: removed rn-switch component

--- components/Switch.tsx
+++ components/Switch.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import Button, { ButtonProps } from '@/components/Button'
-import { ThemedView } from '@/components/ThemedView'
 import { ThemedText } from './ThemedText'
-import { Switch } from 'react-native'
 import { useTheme } from '@react-navigation/native'
-import chroma from 'chroma-js'
+import Animated, { useAnimatedStyle, withSpring } from 'react-native-reanimated'
 
 type Props = ButtonProps & {
   label: string
@@ -24,23 +22,37 @@ const SwitchLabeled: React.FC<Props> = ({
 }) => {
   const { colors } = useTheme()
 
+  const styleDot = useAnimatedStyle(
+    () => ({
+      width: 18,
+      height: 18,
+      borderRadius: 30,
+      backgroundColor: disabled ? '#aaa' : colors.background,
+      left: withSpring(isActived ? 26 : 0, { duration: 500, dampingRatio: 1 }),
+    }),
+    [isActived, disabled, colors]
+  )
+
   return (
     <Button
       disabled={disabled}
       onPress={onPress}
       style={{ flexDirection: 'row', alignItems: 'center' }}
     >
-      <ThemedText style={{ flex: 1, fontSize: 16 }} disabled={disabled}>{label}</ThemedText>
-      <Switch
-        disabled={disabled}
-        value={isActived}
-        onValueChange={() => onPress()}
-        thumbColor={disabled ? undefined : colors.primary}
-        trackColor={{
-          true: disabled ? chroma(colors.text).tint(0.25).hex() : chroma(colors.primary).brighten(2.5).hex(),
-          false: '#666',
+      <ThemedText style={{ flex: 1, fontSize: 16 }} disabled={disabled}>
+        {label}
+      </ThemedText>
+      <Animated.View
+        style={{
+          width: 50,
+          borderRadius: 30,
+          backgroundColor: isActived && !disabled ? colors.primary : '#666',
+          justifyContent: 'center',
+          padding: 3,
         }}
-      />
+      >
+        <Animated.View style={styleDot} />
+      </Animated.View>
       {children}
     </Button>
   )
